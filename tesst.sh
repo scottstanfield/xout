5475a76b99ead4cf1734b276e4ff67f2d95a8632
Scott Stanfield <scott@stanfield.io>
2018-08-22 03:18:04 -0700

    Use color for grep

--- tesst.sh
+++ tesst.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 [ -e big1.txt ] || for i in {1..500}; do cat b6.txt >> big1.txt; done
 
+fgrep --ignore-case --color=auto sanchez big1.txt | head
 python xout.py big1.txt clean.big1.txt
-fgrep sanches clean.big1.txt
+fgrep --ignore-case --color=auto sanchez clean.big1.txt | head
