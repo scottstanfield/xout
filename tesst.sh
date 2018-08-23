01200d68051cf4812ea387d5b62b4a0b8746d869
Scott Stanfield <scott@stanfield.io>
2018-08-22 22:00:51 -0700

    Enable color in diffs

--- tesst.sh
+++ tesst.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
-[ -e big1.txt ] || for i in {1..500}; do cat b6.txt >> big1.txt; done
+#!/bin/bash
+if [[ ! -e "big1.txt" ]]; then
+	echo "making big file"
+	for i in {1..500}; do cat b6.txt >> big1.txt; done
+fi
 
-fgrep --ignore-case --color=auto sanchez big1.txt | head
+fgrep --ignore-case --color=always sanchez big1.txt | head
 python3 xout.py big1.txt clean.big1.txt
-fgrep --ignore-case --color=auto sanchez clean.big1.txt | head
+fgrep --ignore-case --color=always sanchez clean.big1.txt | head
